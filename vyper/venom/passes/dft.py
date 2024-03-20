564b49af56d12b4cc16966cc00309d4b93ef10b1
Charles Cooper <cooper.charles.m@gmail.com>
2024-03-20 16:06:25 -0400

    fix dft pass

--- vyper/venom/passes/dft.py
+++ vyper/venom/passes/dft.py
@@ -7,8 +7,16 @@ from vyper.venom.passes.base_pass import IRPass
 
 class DFTPass(IRPass):
     def _process_instruction_r(self, bb: IRBasicBlock, inst: IRInstruction):
+        for op in inst.get_outputs():
+            for uses_this in self.dfg.get_uses(op):
+                if uses_this.parent != inst.parent or uses_this.fence_id != inst.fence_id:
+                    # don't reorder across basic block or fence boundaries
+                    continue
+                self._process_instruction_r(bb, uses_this)
+
         if inst in self.visited_instructions:
             return
+
         self.visited_instructions.add(inst)
 
         if inst.opcode == "phi":
@@ -17,10 +25,10 @@ class DFTPass(IRPass):
             bb.instructions.append(inst)
             return
 
-        for op in inst.liveness:
+        for op in inst.get_inputs():
             target = self.dfg.get_producing_instruction(op)
             assert target is not None, f"no producing instruction for {op}"
-            if target is None or target.parent != inst.parent or target.fence_id != inst.fence_id:
+            if target.parent != inst.parent or target.fence_id != inst.fence_id:
                 # don't reorder across basic block or fence boundaries
                 continue
             self._process_instruction_r(bb, target)

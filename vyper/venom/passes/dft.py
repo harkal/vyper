93bd5d80cfa5127580bb9add9b3829b2dae12d1e
Charles Cooper <cooper.charles.m@gmail.com>
2024-03-22 15:43:23 -0400

    rewrite dft pass again

--- vyper/venom/passes/dft.py
+++ vyper/venom/passes/dft.py
@@ -6,7 +6,7 @@ from vyper.venom.passes.base_pass import IRPass
 
 
 class DFTPass(IRPass):
-    def _process_instruction_r(self, bb: IRBasicBlock, inst: IRInstruction, offset: int = 0):
+    def _process_instruction_r(self, bb: IRBasicBlock, inst: IRInstruction, pin: IRInstruction):
         for op in inst.get_outputs():
             assert isinstance(op, IRVariable), f"expected variable, got {op}"
             uses = self.dfg.get_uses(op)
@@ -16,11 +16,7 @@ class DFTPass(IRPass):
                     # don't reorder across basic block or fence boundaries
                     continue
 
-                # if the instruction is a terminator, we need to place it at the end of the basic block
-                # along with all the instructions that "lead" to it
-                if uses_this.opcode in BB_TERMINATORS:
-                    offset = len(bb.instructions)
-                self._process_instruction_r(bb, uses_this, offset)
+                self._process_instruction_r(bb, uses_this, pin)
 
         if inst in self.visited_instructions:
             return
@@ -29,8 +25,7 @@ class DFTPass(IRPass):
         if inst.opcode == "phi":
             # phi instructions stay at the beginning of the basic block
             # and no input processing is needed
-            # bb.instructions.append(inst)
-            self.inst_order[inst] = 0
+            bb.instructions.append(inst)
             return
 
         for op in inst.get_inputs():
@@ -39,10 +34,14 @@ class DFTPass(IRPass):
             if target.parent != inst.parent or target.fence_id != inst.fence_id:
                 # don't reorder across basic block or fence boundaries
                 continue
-            self._process_instruction_r(bb, target, offset)
+            self._process_instruction_r(bb, target, pin)
 
-        self.inst_order_num += 1
-        self.inst_order[inst] = self.inst_order_num + offset
+        # force terminating instruction to come after everything else in the block
+        if inst.opcode in BB_TERMINATORS:
+            for target in pin:
+                self._process_instruction_r(bb, target, pin)
+
+        bb.instructions.append(inst)
 
     def _process_basic_block(self, bb: IRBasicBlock) -> None:
         self.ctx.append_basic_block(bb)
@@ -55,12 +54,10 @@ class DFTPass(IRPass):
         # We go throught the instructions and calculate the order in which they should be executed
         # based on the data flow graph. This order is stored in the inst_order dictionary.
         # We then sort the instructions based on this order.
-        self.inst_order = {}
-        self.inst_order_num = 0
-        for inst in bb.instructions:
-            self._process_instruction_r(bb, inst)
-
-        bb.instructions.sort(key=lambda x: self.inst_order[x])
+        instructions = bb.instructions
+        bb.instructions = []
+        for inst in instructions:
+            self._process_instruction_r(bb, inst, instructions)
 
     def _run_pass(self, ctx: IRFunction) -> None:
         self.ctx = ctx

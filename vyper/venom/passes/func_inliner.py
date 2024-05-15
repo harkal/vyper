c16cde0668cc148587ab8e8295c92d70b2ad3420
Harry Kalogirou <harkal@nlogn.eu>
2024-05-15 23:59:12 +0300

    Inline single-call functions

--- vyper/venom/passes/func_inliner.py
+++ vyper/venom/passes/func_inliner.py
@@ -10,13 +10,34 @@ class FuncInlinerPass(IRPass):
 
     def run_pass(self):
         ctx = self.function.ctx
-        func_call_counts = {fn: 0 for fn in ctx.functions}
+        func_call_sites = {fn: [] for fn in ctx.functions}
         
         for bb in ctx.get_basic_blocks():
             for inst in bb.instructions:
                 if inst.opcode == "invoke":
                     func_name = inst.operands[0]
-                    func_call_counts[func_name] += 1
+                    func_call_sites[func_name].append(inst)
 
-        print(func_call_counts)
+        funcs = self._filter_candidates(func_call_sites)
+        for func in funcs:
+            self._inline_function(func, func_call_sites[func])
+        
+    def _filter_candidates(self, func_call_counts):
+        """
+        Filter candidates for inlining. This will become more sophisticated in the future.
+        """
+        return [fn for fn, call_sites in func_call_counts.items() if len(call_sites) == 1]
+    
+    def _inline_function(self, func, call_sites):
+        """
+        Inline function into call sites.
+        """
+        for call_site in call_sites:
+            self._inline_call_site(func, call_site)
+
+    def _inline_call_site(self, func, call_site):
+        """
+        Inline function into call site.
+        """
+        bb = call_site.parent
         
